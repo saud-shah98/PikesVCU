7dc7d9a60db69afa8a0283bd7ad69dc4549554a1
MSS <mss@MSSs-MBP.cable.rcn.com>
2021-01-15 23:24:45 -0600

    public

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(9000);
