735d01cd3ff184df26694a3b30794fce57ec5067
MSS <mss@MSSs-MBP.cable.rcn.com>
2021-01-16 00:29:36 -0600

    public

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express(); // create express app
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
