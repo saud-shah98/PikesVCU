096dac9ec8ced0f1b5fb2898c54b76280e9ef252
MSS <mss@MSSs-MBP.cable.rcn.com>
2021-01-15 23:22:04 -0600

    static.json

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,21 @@ import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
 
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
+
+
+
 class IndexPage extends React.Component {
   constructor(props) {
     super(props)
