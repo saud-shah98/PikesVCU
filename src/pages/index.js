48eb105e57d30c5522b7c1de38ef66a5142bec97
MSS <mss@MSSs-MBP.cable.rcn.com>
2021-01-16 00:20:20 -0600

    public

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,6 @@ import Main from '../components/Main'
 import Footer from '../components/Footer'
 
 
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.listen(9000);
-
-
 
 class IndexPage extends React.Component {
   constructor(props) {
